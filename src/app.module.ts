29eb645f4989a3fb9717c655b3ada5962d625315
SofiaBartoli <sofiadbartoli@gmail.com>
2026-03-18 20:06:54 -0300

    Agrego modulo interactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ClientsModule } from './clients/clients.module';
+import { InteractionsModule } from './interactions/interactions.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ClientsModule],
+  imports: [PrismaModule, AuthModule, ClientsModule, InteractionsModule],
 })
 export class AppModule {}
